42ca93ee17c0541dc7e0ff295adcebbdc879ce0e
ir0nstrik3 <50269904+ir0nstrik3@users.noreply.github.com>
2023-08-19 14:22:48 +0200

    edit

--- Mal-Dow-Exe.py
+++ Mal-Dow-Exe.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-import csv
 import os
 import requests
 import time
 import pyzipper
+import json
 
 file_exe_validation = input("\033[1;31;40m please say Yes or No in uppercase Letters to automatic executing the samples: \033[0;0m")
 vali_choices = ["NO","YES"]
@@ -12,32 +12,11 @@ vali_choices = ["NO","YES"]
 while file_exe_validation not in vali_choices:
     file_exe_validation = input("\033[1;31;40m please say Yes or No in uppercase Letters to automatic executing the samples: \033[0;0m")
 
+malware_samples = input("please set a valid number of malware samples you want to download: ")
 
-csv_file = input("please set the path to csv file: ")
-csv_validation = False
-line = 0
-
-while os.path.isfile(csv_file) == False and ".csv" not in csv_file:
-   csv_file = input("please set the path to csv file again !: ")
-
-if os.path.isfile(csv_file) == True:
-
-    with open (csv_file, mode = 'r') as file:
-        csv_file_true_parser = csv.reader(file)
-        csv_list_parser = list(csv_file_true_parser)
-        error_count = 0
-        
-        for url_list_parser_vali in csv_list_parser:
-            if len(url_list_parser_vali[0]) == 64:
-                line += 1
-            else:
-                csv_validation = False
-                line += 1
-                error_count += 1
-                print(f"The csv file has error in Line {line} please check it !")
-        
-        if error_count > 0:
-            quit()
+while malware_samples.isnumeric() == False:
+    malware_samples = input("please set a valid number of malware samples you want to download: ")
+           
 current_path = str(os.path.dirname(os.path.realpath(__file__))) + "/"
 dir_name = "Samples"
 
@@ -48,24 +27,34 @@ if os.path.isdir(current_path + dir_name) != True:
 
 download_path = str(current_path) + "Samples/"
 
-with open (csv_file, mode = 'r') as file:
-    csvFile = csv.reader(file)
-    csv_list = list(csvFile)
-    malware_count = 0
-    
-for hash_list in csv_list:
-    
-    malware_count += 1
-    download_file = requests.post('https://mb-api.abuse.ch/api/v1/', data={'query': 'get_file', 'sha256_hash': hash_list[0]})
+download_file_json = requests.post('https://mb-api.abuse.ch/api/v1/', data={'query': 'get_file_type', 'file_type': 'exe', 'limit': malware_samples})
+open(download_path + "Malware_samples.json", 'wb').write(download_file_json.content)
+
+malware_sha256_hash_list = []
+
+json_file = open(download_path + "Malware_samples.json")
+json_data = json.load(json_file)
+for i in json_data['data']:
+    malware_sha256_hash_list.append(i['sha256_hash'])
+json_file.close()
+
+malware_count = 0
+
+while malware_count < int(malware_samples):
+    sha256 = malware_sha256_hash_list[malware_count]
+    download_file = requests.post('https://mb-api.abuse.ch/api/v1/', data={'query': 'get_file', 'sha256_hash': sha256})
     open(download_path + "Malware_" + str(malware_count) + ".zip", 'wb').write(download_file.content)
-        
+      
     with pyzipper.AESZipFile(download_path + "Malware_" + str(malware_count) + ".zip", 'r', compression=pyzipper.ZIP_DEFLATED, encryption=pyzipper.WZ_AES) as zip_ref:
         zip_ref.extractall(download_path , members=None, pwd=b'infected')
 
     os.remove(download_path + "Malware_" + str(malware_count) + ".zip")
-    print(f"file {malware_count} of {len(csv_list)} is downloaded now !", end="\r")
+    print(f"file {malware_count} of {len(malware_sha256_hash_list)} is downloaded now !", end="\r")
     time.sleep(0.5)
-      
+    malware_count += 1
+
+os.remove(download_path + "Malware_samples.json")
+ 
 print("\n")
 print("0.5 min later it will let you know how much files are left")
 time_count = 0
